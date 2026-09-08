76e27ccc3882930de1a9bb2511d59b56d26880c1
muhsin-ahamed <muhsinahamed411@gmail.com>
2026-09-08 17:15:21 +0530

    Update web build with latest changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4026307953" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3579498216" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
